fbf1f305f11420067f70f13de55d017cce216330
cp985 <166384434+cp985@users.noreply.github.com>
2026-05-22 17:49:04 +0200

    fix dropdown menu 7?

--- src/components/landingpage/DropDownProfile.tsx
+++ src/components/landingpage/DropDownProfile.tsx
@@ -59,20 +59,20 @@ export default function DropDownProfile() {
 
   return (
     <div className="flex items-center gap-4">
-      <DropdownMenu open={isOpen} onOpenChange={setIsOpen} >
+      <DropdownMenu open={isOpen} onOpenChange={setIsOpen} modal={false} >
        
         <DropdownMenuTrigger asChild>
           <Button
             variant="ghost"
             className={cn(
-              "relative h-12! w-12! md:h-14! md:w-14! rounded-sm p-0 overflow-hidden",
+              "relative h-11! w-11! md:h-13! md:w-13! rounded-sm p-0 overflow-hidden",
               "border border-cyan-500/20 bg-[#050A14]/50 backdrop-blur-md",
               "hover:border-cyan-500/50 hover:bg-cyan-950/30 transition-all",
               "focus-visible:ring-1 focus-visible:ring-cyan-500 focus-visible:ring-offset-0"
             )}
           >
             <div className="flex bg-[#1e3a8a14]! flex-col items-center justify-center w-full h-full pb-3 md:pb-3.5">
-              <FaUserAstronaut className="w-5 h-5 md:w-6 md:h-6 text-cyan-400/80 transition-all" />
+              <FaUserAstronaut className="w-5! h-5! md:w-7! md:h-7! text-cyan-400/80 transition-all" />
               
               <div className="absolute bottom-0 inset-x-0 h-3.5 md:h-4 bg-cyan-950/80 backdrop-blur-sm border-t border-cyan-500/20 flex items-center justify-center">
                 <span className="text-[9px] md:text-[10px] font-mono tracking-widest text-cyan-200/80 truncate px-1 transition-all">
